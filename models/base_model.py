2491a16dd68d3e67a312169d7908f8c79ded501f
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-12-06 12:45:18 -0800

    create base model from dict

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
