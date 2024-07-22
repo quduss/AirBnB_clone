2e4cb8aeb0c3dd14ae12fe50725edad8f90a7240
quduss <shuaibqudus33@gmail.com>
2024-07-22 18:57:37 +0100

    Remove unnecessary code

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
 """__init__ file for the application"""
 
-from .engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
-
-classes = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "Review": Review,
-        "Place": Place,
-        "Amenity": Amenity,
-        "State": State,
-        "City": City
-        }
