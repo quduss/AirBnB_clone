7f13de9b921af61dff5127e874945f508f548d6a
quduss <shuaibqudus33@gmail.com>
2024-07-23 15:49:15 +0100

    change storage.all() to earlier declared my_dict

--- console.py
+++ console.py
@@ -141,9 +141,9 @@ class HBNBCommand(cmd.Cmd):
         try:
             if args[0] in classes:
                 for key in my_dict:
-                    to_dict = storage.all()[key].to_dict()
+                    to_dict = my_dict[key].to_dict()
                     if args[0] == to_dict["__class__"]:
-                        my_list.append(str(storage.all()[key]))
+                        my_list.append(str(my_dict[key]))
                 print(my_list)
             else:
                 cne()
